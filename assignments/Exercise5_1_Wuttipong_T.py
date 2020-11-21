1de272694abc3efc2e98603189a5699604bedf14
Posvite <74806344+Posvite@users.noreply.github.com>
2020-11-21 20:40:58 +0700

    Create Exercise5_1_Wuttipong_T.py

---
+++ assignments/Exercise5_1_Wuttipong_T.py
@@ -0,0 +1,6 @@
+Number1 = int(input("ตัวเลขที่ 1 >"))  #ใส่เลข 10
+Number2 = int(input("ตัวเลขที่ 2 >"))  #ใส่เลข 2
+print(Number1,"+",Number2,"=",Number1 + Number2)
+print(Number1,"-",Number2,"=",Number1 - Number2)
+print(Number1,"*",Number2,"=",Number1 * Number2)
+print(Number1,"/",Number2,"=",int(Number1 / Number2))
