826674b195441defccd57b856a128388bcd9415d
Posvite <74806344+Posvite@users.noreply.github.com>
2020-11-22 18:46:19 +0700

    Create Lecture-50-Wuttipong-T.py

---
+++ assignments/Lecture-50-Wuttipong-T.py
@@ -0,0 +1,14 @@
+def addNumber(x, y):
+    print(x+y)
+def delNumber(x, y):
+    print(x-y)
+def mulNumper(x, y):
+    print(x*y)
+def divNumber(x, y):
+    print(x/y)
+
+
+addNumber(10,2)
+delNumber(10,2)
+mulNumper(10,2)
+divNumber(10,2)
